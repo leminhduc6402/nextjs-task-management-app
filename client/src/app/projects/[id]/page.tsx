f34d01c49cd7327e4c25a119aeb2f6e2c23b1f0b
leminhduc6402 <leminhduc6402@gmail.com>
2024-11-25 20:08:49 +0700

    Create modal add project and task

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -4,6 +4,8 @@ import ProjectHeader from "../ProjectHeader";
 import BoardView from "../BoardView";
 import ListView from "../ListView";
 import Timeline from "../TimelineView";
+import Table from "../TableView";
+import ModalNewTask from "@/components/ModalNewTask";
 
 type Props = {
     params: { id: string };
@@ -13,9 +15,13 @@ function Project({ params }: Props) {
     const { id } = params;
     const [activeTab, setActiveTab] = useState("Board");
     const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
-
     return (
         <>
+            <ModalNewTask
+                isOpen={isModalNewTaskOpen}
+                onClose={() => setIsModalNewTaskOpen(false)}
+                id={id}
+            />
             <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
             {activeTab === "Board" && (
                 <BoardView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
@@ -25,6 +31,9 @@ function Project({ params }: Props) {
             )}
             {activeTab === "Timeline" && (
                 <Timeline id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+            )}
+            {activeTab === "Table" && (
+                <Table id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
             )}
         </>
     );
---
+++ client/src/lib/utils.ts
@@ -0,0 +1,35 @@
+export const dataGridClassName = `
+border border-gray-200 bg-white shadow
+dark:border-stroke-dark dark:text-white
+dark:bg-dark-secondary
+`;
+
+export const dataGridSxStyles = (isDarkMode: boolean) => {
+    return {
+        "& .MuiDataGrid-columnHeaders": {
+            color: `${isDarkMode ? "#e5e7eb" : ""}`,
+            '& [role="row"] > *': {
+                backgroundColor: `${isDarkMode ? "#1d1f21" : "white"}`,
+                borderColor: `${isDarkMode ? "#2d3135" : ""}`,
+            },
+        },
+        "& .MuiIconbutton-root": {
+            color: `${isDarkMode ? "#a3a3a3" : ""}`,
+        },
+        "& .MuiTablePagination-root": {
+            color: `${isDarkMode ? "#a3a3a3" : ""}`,
+        },
+        "& .MuiTablePagination-selectIcon": {
+            color: `${isDarkMode ? "#a3a3a3" : ""}`,
+        },
+        "& .MuiDataGrid-cell": {
+            border: "none",
+        },
+        "& .MuiDataGrid-row": {
+            borderBottom: `1px solid ${isDarkMode ? "#2d3135" : "#e5e7eb"}`,
+        },
+        "& .MuiDataGrid-withBorderColor": {
+            borderColor: `${isDarkMode ? "#2d3135" : "#e5e7eb"}`,
+        },
+    };
+};
